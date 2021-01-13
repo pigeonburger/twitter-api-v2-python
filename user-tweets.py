707b65a50d048719899f46dcb132868dac3a6cf1
pygeonburger <70826123+pygeonburger@users.noreply.github.com>
2021-01-13 14:47:26 +1100

    Create user-tweets.py

---
+++ user-tweets.py
@@ -0,0 +1,33 @@
+# THIS SCRIPT ALLOWS YOU TO GET THE TWEETS OF A SPECIFIC USER USING THE TWITTER API V2
+
+# Import required libraries
+import requests, configparser
+from requests_oauthlib import OAuth1
+
+# Read the keys from auth.ini and define them
+config = configparser.ConfigParser()
+config.read('auth.ini')
+
+CONSUMER_KEY = config.get('credentials', 'consumer_key')
+CONSUMER_SECRET = config.get('credentials', 'consumer_secret')
+ACCESS_TOKEN = config.get('credentials', 'access_token')
+ACCESS_TOKEN_SECRET = config.get('credentials', 'access_token_secret')
+
+oauth = OAuth1(CONSUMER_KEY,
+  client_secret=CONSUMER_SECRET,
+  resource_owner_key=ACCESS_TOKEN,
+  resource_owner_secret=ACCESS_TOKEN_SECRET)
+
+params = {"tweet.fields": "created_at"}
+
+user_id = 1299121724684869633
+url = f"https://api.twitter.com/2/users/{user_id}/tweets"
+
+response = requests.get(url, params=params, auth=oauth)
+
+result = response.json()
+
+# Print the response
+print(result)
+
+# Do whatever you want with the result here after.
