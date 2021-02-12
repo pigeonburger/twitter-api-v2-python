b8111ea8323c93bc01c4a9ddf26d75ee1a2be1e2
pygeonburger <70826123+pygeonburger@users.noreply.github.com>
2021-02-12 17:05:21 +1100

    Create followers-list.py

---
+++ followers-list.py
@@ -0,0 +1,29 @@
+# THIS SCRIPT ALLOWS YOU TO RETURN THE A LIST OF USER OBJECTS FOR PEOPLE THAT FOLLOW THE SPECIFIED USER
+
+# Import required libraries
+import requests, configparser
+from requests_oauthlib import OAuth1
+
+# Read the keys from auth.ini and define them
+config = configparser.ConfigParser()
+config.read('auth.ini')
+
+CONSUMER_KEY = config.get('credentials', 'consumer_key')
+CONSUMER_SECRET = config.get('credentials', 'consumer_secret')
+ACCESS_TOKEN = config.get('credentials', 'access_token')
+ACCESS_TOKEN_SECRET = config.get('credentials', 'access_token_secret')
+
+oauth = OAuth1(CONSUMER_KEY,
+  client_secret=CONSUMER_SECRET,
+  resource_owner_key=ACCESS_TOKEN,
+  resource_owner_secret=ACCESS_TOKEN_SECRET)
+
+# Add a Twitter username here
+params = {'screen_name': 'editvideobot'}
+
+response = requests.get("https://api.twitter.com/1.1/followers/list.json", params=params, auth=oauth)
+
+# Print the response
+print(response.json())
+
+# Do whatever you want with the result here after.
